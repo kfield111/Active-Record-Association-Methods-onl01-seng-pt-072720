3fd451c9a2145c1acf27dc8e8c4d8a7d02af3ada
Kelin Field <kfield111@hotmail.com>
2020-10-19 00:21:46 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    new_artist = Artist.find_or_create_by(name) do |t|
+    new_artist = Artist.find_or_create_by do |t|
       new_artist.name = "Drake"
       new_artist.save
     end
